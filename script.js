9850ae0b512ac695f227d3e98323dc95cfdb41c0
JoeTheorium <fernandoandresraggio@gmail.com>
2023-04-21 01:24:44 -0300

    Actualización

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Agarramos elementos del DOM
+// Constantes
 const screen = document.querySelector("#screen");
 const calculate = document.querySelector("#calculate");
 const result = document.querySelector("#result");
@@ -13,6 +14,7 @@ const equalBtn = document.querySelector("#equal");
 const decimalBtn = document.querySelector("#decimal");
 const numbersBtn = document.querySelectorAll(".number");
 
+// Variables
 let firstValue = "";
 let secondValue = "";
 let currentOperation = null;
